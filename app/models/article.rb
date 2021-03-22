6cdabcfd6c9abfa72db645ad0f0fcbcfa48e2558
Erin Anderson <e.anderson1226@gmail.com>
2021-03-21 20:54:33 -0400

    commit tree f6f965a6b4be923fff240b0de0cb0ef431d512ed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,20 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+
+    #create
+    get '/article/new' do
+        erb :new
+    end
+
+    #read
+
+    #new
+
+    #show
+
+    #update
+
+    #delete
+    
 end
